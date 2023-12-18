64dfd34db7ea6712a5ecda4f92c2480d8a9d194d
Lucas Santos <ls6865316@gmail.com>
2023-12-18 12:14:29 -0300

    feat: "service registry criado"

---
+++ src/main/java/com/ead/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.ead.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// para habilitar o servidor eureka
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
